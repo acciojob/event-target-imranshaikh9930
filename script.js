2f7def675dd3d80957d99ae90fcb305301718f33
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-05-11 12:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,32 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+
+		if(!this.listeners.has[event]){
+
+			this.listeners.set(event,[]);
+		}
+
+		const listeners - this.listeners.get(event);
+		if(!listeners.includes(callback)){
+			listeners.push(callback);
+		}
     }
   
     removeEventListener(event, callback) {
-      
+      if (this.listeners.has(event)) {
+            const listeners = this.listeners.get(event).filter(listener => listener !== callback);
+            this.listeners.set(event, listeners);
+        }
     }
   
     dispatchEvent(event) {
-    
+    if (this.listeners.has(event)) {
+            const listeners = this.listeners.get(event);
+            listeners.forEach(callback => {
+                callback();
+            });
+        }
     }
     
   }
